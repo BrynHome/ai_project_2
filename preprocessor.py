2c34d1f8e7369b80d3c133dfc26998e1dd5f72e4
Bryan <b.r.hill@outlook.com>
2023-03-16 18:23:50 -0700

    Added Cleaning step in prep for training data set

--- preprocessor.py
+++ preprocessor.py
@@ -19,7 +19,9 @@ text: input. could be skimmed or altered to make input easier?
 date: to be removed/ignored
 
 """
-from pandas import read_json, json_normalize
+import os.path
+
+from pandas import read_json, json_normalize, read_csv, DataFrame
 
 class DataPreprocessor:
     """
@@ -29,13 +31,25 @@ class DataPreprocessor:
     def __init__(self):
         pass
 
+    def training_clean(self, data: DataFrame) -> DataFrame:
+        """
+            Clean the training dataset.
+            Removes entries that have no values for:
+                stars
+                useful
+                funny
+                cool
+                text
+            THIS IS ONLY FOR THE TRAINING SET
+        """
+        for label in ["stars", "useful", "funny", "cool"]:
+            data.drop(data[data[label] == '0'].index, inplace=True)
+        return data
+
     def process(self, data):
         """
             Preprocesses the given dataset:
                 review_id, user_id, business_id     -> Dropped from the dataset. They carry no relevant information
-
-        :param data:
-        :return:
         """
 
         data = data.drop(labels="review_id", axis=1)
@@ -44,12 +58,18 @@ class DataPreprocessor:
         # may not drop this label
         data = data.drop(labels="business_id", axis=1)
 
+        data["text"] = data["text"].fillna(0)
         return data
 
 if __name__ == '__main__':
-    d = read_json('yelp_academic_dataset_review.json', lines=True, chunksize=10000)
-    print("data read")
-    for data in d:
-        preprocessor = DataPreprocessor()
-        result = preprocessor.process(data)
-        result.to_csv('out.csv', mode='a')
+    preprocessor = DataPreprocessor()
+    if os.path.exists('out.csv'):
+        d = read_csv('out.csv')
+        res = preprocessor.training_clean(d)
+        res.to_csv('clean.csv', mode='w+', index=False)
+    else:
+        d = read_json('yelp_academic_dataset_review.json', lines=True, chunksize=10000)
+        print("data read")
+        for data in d:
+            result = preprocessor.process(data)
+            result.to_csv('out.csv', mode='a', index=False)
