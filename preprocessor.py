abaac67424a74d620fd7aac3d25295f12214773a
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-03-20 15:25:45 -0700

    Refactored the preprocessing script, building on top of what Bryan had done.

--- preprocessor.py
+++ preprocessor.py
@@ -1,75 +1,79 @@
 """
 Format of record analysis
 
-review_id : likely unimportant. drop
-user_id : maybe something, but likely to result in false positives? likely drop/ignore
-business_id: maybe something. but unlikely. Certain restaurants may be more likely to
-    rate less from their previous scores? likely drop, but not 100%
-stars: output. to be dropped but kept for training.
-useful, funny, cool : a large majority of these ratings are default not rated, as
-    such, only records that have these values should be used for training.
-    This will likely end up in poor accuracy for these, as it is very likely a record
-    has no rating of these.
+    stars: 
+        output. to be dropped but kept for training.
+    
+    useful, funny, cool : 
+        a large majority of these ratings are default not rated, as
+        such, only records that have these values should be used for training.
+        This will likely end up in poor accuracy for these, as it is very likely a record
+        has no rating of these.
 
-text: input. could be skimmed or altered to make input easier?
-    words like: 'the', 'a' , 'my', 'it', etc.. are likely unimportant so may be removed
+    text: 
+        input. could be skimmed or altered to make input easier?
+        words like: 'the', 'a' , 'my', 'it', etc.. are likely unimportant so may be removed
         tests with skim and no skim should be done
-    punctuation is likely not important
-
-date: to be removed/ignored
-
+        punctuation is likely not important
 """
-import os.path
+from pandas import DataFrame, read_json, read_csv, concat
+from argparse import ArgumentParser
+from sklearn.model_selection import train_test_split
+from os.path import exists
+from os import remove
 
-from pandas import read_json, json_normalize, read_csv, DataFrame
+PARSER = ArgumentParser()
+PARSER.add_argument("filepath", help="The filepath to the raw JSON dataset.")
+ARGS = PARSER.parse_args()
 
-class DataPreprocessor:
-    """
-    Preprocessor for the yelp_academic_dataset_review.json dataset
-    """
+RAW_DATASET = "dataset.csv"
 
-    def __init__(self):
-        pass
+if __name__ == "__main__":
 
-    def training_clean(self, data: DataFrame) -> DataFrame:
-        """
-            Clean the training dataset.
-            Removes entries that have no values for:
-                stars
-                useful
-                funny
-                cool
-                text
-            THIS IS ONLY FOR THE TRAINING SET
-        """
-        for label in ["stars", "useful", "funny", "cool"]:
-            data.drop(data[data[label] == '0'].index, inplace=True)
-        return data
+    if exists(RAW_DATASET):
+        remove(RAW_DATASET)
 
-    def process(self, data):
-        """
-            Preprocesses the given dataset:
-                review_id, user_id, business_id     -> Dropped from the dataset. They carry no relevant information
-        """
+    print("Converting raw JSON dataset to CSV...")
+    reader = read_json(ARGS.filepath, lines=True, chunksize=300000)
+    for chunk in reader:
+        chunk = chunk[["stars", "useful", "funny", "cool", "text"]]
+        chunk.to_csv(RAW_DATASET, mode="a", index=False)
 
-        data = data.drop(labels="review_id", axis=1)
-        data = data.drop(labels="user_id", axis=1)
-        data = data.drop(labels="date", axis=1)
-        # may not drop this label
-        data = data.drop(labels="business_id", axis=1)
+    # I find it a lot easier to
+    # process data when it is a 
+    # string.
+    COLUMN_TYPES = {
+        "text": str,
+        "useful": str,
+        "funny": str,
+        "cool": str,
+        "stars": str
+    }
 
-        data["text"] = data["text"].fillna(0)
-        return data
+    CLASS_LABELS = ["stars", "useful", "funny", "cool"]
+    full = read_csv(RAW_DATASET, dtype=COLUMN_TYPES)
+    test = full[full["text"] != ""] # Get all rows with non-empty text fields.
+    print("Splitting dataset into training and test sets...")
+    X_train, X_test, y_train, y_test = train_test_split(full["text"], full[CLASS_LABELS], test_size=0.10, random_state=42)
 
-if __name__ == '__main__':
-    preprocessor = DataPreprocessor()
-    if os.path.exists('out.csv'):
-        d = read_csv('out.csv')
-        res = preprocessor.training_clean(d)
-        res.to_csv('clean.csv', mode='w+', index=False)
-    else:
-        d = read_json('yelp_academic_dataset_review.json', lines=True, chunksize=10000)
-        print("data read")
-        for data in d:
-            result = preprocessor.process(data)
-            result.to_csv('out.csv', mode='a', index=False)
+    # Create the test set.
+    print("Saving test set...")
+    test: DataFrame = concat([X_test, y_test], axis=1)
+    test.to_csv("test.csv", index=False)
+
+    # Create a training sets.
+    training = concat([X_train, y_train], axis=1)
+    for label in CLASS_LABELS:
+        print(f"Creating/preprocessing training set for label: {label}")
+        # This seemed like the simplest way to 
+        # ensure that the rating column only 
+        # contains numbers 1 through 5.
+        data = training[
+            (training[label] == "1") |
+            (training[label] == "2") |
+            (training[label] == "3") |
+            (training[label] == "4") |
+            (training[label] == "5") 
+            ]
+        data[["text", label]].to_csv(f"training_{label}.csv", index=False)
+    print("Done.")
