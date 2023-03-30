2325375137ba651340103c92a6ec541233fae80b
Bryan <b.r.hill@outlook.com>
2023-03-29 17:03:02 -0700

    fixed small issues

--- neural_net_train.py
+++ neural_net_train.py
@@ -2,15 +2,15 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
+from pandas import DataFrame
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import AutoTokenizer
 from bh_neural_net_simple import Dataset, Classify
 from sklearn.model_selection import train_test_split
-import tensorflow
-from transformers import TFRobertaModel
+import tensorflow as tf
+from transformers import TFAutoModelForSequenceClassification
 import warnings
-warnings.filterwarnings("ignore")
 
 
 def encode(texts, tokenizer):
@@ -35,137 +35,48 @@ def encode(texts, tokenizer):
     }
 
 
-def model_make(cat, strategy):
-    max = 256
-    with strategy.scope():
-        input_word_ids = tensorflow.keras.Input((max,), name='input_word_ids', dtype=tensorflow.int32)
-        input_mask = tensorflow.keras.Input((max,), name='input_mask', dtype=tensorflow.int32)
-        input_type_ids = tensorflow.keras.Input((max,), name='input_type_ids', dtype=tensorflow.int32)
+def model_make(cat):
+    m = TFAutoModelForSequenceClassification.from_pretrained("roberta-base", num_labels=cat)
+    m.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5, clipnorm=1.),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[tf.metrics.SparseCategoricalAccuracy()],
+        run_eagerly=True
+    )
+    return m
 
-        r_model = TFRobertaModel.from_pretrained('roberta-base')
-        r_model_2 = r_model(input_word_ids, attention_mask=input_mask, token_type_ids=input_type_ids)
-        r_model_2 = r_model_2[0]
-
-        r_model_2 = tensorflow.keras.layers.Dropout(0.1)(r_model_2)
-        r_model_2 = tensorflow.keras.layers.Flatten()(r_model_2)
-        r_model_2 = tensorflow.keras.layers.Dense(256, 'relu')(r_model_2)
-        r_model_2 = tensorflow.keras.layers.Dense(cat, 'softmax')(r_model_2)
-
-        r_model_3 = tensorflow.keras.Model(inputs=[input_word_ids, input_mask, input_type_ids], outputs=r_model_2)
-        r_model_3.compile(tensorflow.keras.optimizers.Adam(1e-5),
-                          'sparse_categorical_crossentropy',
-                          metrics=['accuracy'])
-        return r_model_3
-
-
-def train(model, train_dl, val_dl, learn_rate, epochs):
-    best_loss = float('inf')
-    early_stop_count = 0
-
-    cuda = torch.cuda.is_available()
-    device = torch.device("cuda" if cuda else "cpu")
-
-    criterion = nn.BCELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate)
-
-    model = model.to(device)
-    criterion = criterion.to(device)
-    for epoch in range(epochs):
-        total_acc_train = 0
-        total_loss_train = 0
-
-        model.train()
-        for train_in, train_label in tqdm(train_dl):
-            attn_mask = train_in["attention_mask"].to(device)
-            input_ids = train_label["input_ids"].squeeze(1).to(device)
-            train_label = train_label.to(device)
-
-            out = model(input_ids, attn_mask)
-            loss = criterion(out, train_label.float().unsqueeze(1))
-
-            total_loss_train += loss.item()
-            acc = ((out >= 0.5).int() == train_label.unsqueeze(1)).sum()
-            total_acc_train += acc
-
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-        with torch.no_grad():
-            total_acc_val = 0
-            total_loss_val = 0
-
-            model.eval()
-
-            for val_input, val_label in tqdm(val_dl):
-                attention_mask = val_input['attention_mask'].to(device)
-                input_ids = val_input['input_ids'].squeeze(1).to(device)
-
-                val_label = val_label.to(device)
-
-                output = model(input_ids, attention_mask)
-
-                loss = criterion(output, val_label.float().unsqueeze(1))
-
-                total_loss_val += loss.item()
-
-                acc = ((output >= 0.5).int() == val_label.unsqueeze(1)).sum().item()
-                total_acc_val += acc
-
-            print(f'Epochs: {epoch + 1} '
-                  f'| Train Loss: {total_loss_train / len(train_dl): .3f} '
-                  f'| Train Accuracy: {total_acc_train / (len(train_dl.dataset)): .3f} '
-                  f'| Val Loss: {total_loss_val / len(val_dl): .3f} '
-                  f'| Val Accuracy: {total_acc_val / len(val_dl.dataset): .3f}')
-
-            if best_val_loss > total_loss_val:
-                best_val_loss = total_loss_val
-                torch.save(model, f"bh_model.pt")
-                print("Saved model")
-                early_stopping_threshold_count = 0
-            else:
-                early_stopping_threshold_count += 1
-
-            if early_stopping_threshold_count >= 1:
-                print("Early stopping")
-                break
 
 
 if __name__ == "__main__":
-    try:
-        # TPU detection. No parameters necessary if TPU_NAME environment variable is set (always set in Kaggle)
-        tpu = tensorflow.distribute.cluster_resolver.TPUClusterResolver()
-        tensorflow.config.experimental_connect_to_cluster(tpu)
-        tensorflow.tpu.experimental.initialize_tpu_system(tpu)
-        strategy = tensorflow.distribute.experimental.TPUStrategy(tpu)
-        print('Running on TPU ', tpu.master())
-    except ValueError:
-        # Default distribution strategy in Tensorflow. Works on CPU and single GPU.
-        strategy = tensorflow.distribute.get_strategy()
 
-    print('Number of replicas:', strategy.num_replicas_in_sync)
+    batch_size = 32
+    print('Batch size:', batch_size)
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
     print("Reading dataset...")
     train_df = pd.read_csv("data/training.csv")
-    n = len(train_df['stars'].unique())
+    n = len(train_df['stars'])
+    n2 = int(n*0.99)
+    print(n)
     train_df = train_df.dropna()
-    train_df, val_df = train_test_split(train_df, test_size=0.2, random_state=42)
-    x_train = train_df[['text']].values.reshape(-1)
-    y_train = train_df[['stars']].values.reshape(-1)
-    x_val = val_df[['text']].values.reshape(-1)
-    y_val = val_df[['stars']].values.reshape(-1)
+    train_df = train_df.drop(np.random.choice(train_df.index, n2, replace=False))
+    print(len(train_df['stars']))
+    texts = train_df['text'].tolist()
+    labels = train_df['stars'].tolist()
+    categories = sorted(list(set(labels)))  # set will return the unique different entries
+    n_categories = len(categories)
     print("Grabbing Tokenizer...")
     tokenizer = AutoTokenizer.from_pretrained("roberta-base")
-
-    print("Creating Dataloaders...")
-    x_train = encode(x_train, tokenizer)
-    x_val = encode(x_val, tokenizer)
-    y_train = np.asarray(y_train, dtype='int32')
-    y_val = np.asarray(y_val, dtype='int32')
+    input = tokenizer(texts, padding=True, truncation=True, return_tensors='tf')
+    data = tf.data.Dataset.from_tensor_slices((dict(input), labels))
+    val_s = int(0.2*(n-n2))
+    val_ds = data.take(val_s).batch(32, drop_remainder=True)
+    train_ds = data.skip(val_s).batch(32, drop_remainder=True)
+    train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
 
     print("Making model...")
-    model = model_make(n, strategy)
+    model = model_make(n_categories)
 
     print("Training....")
-    with strategy.scope():
-        h = model.fit(x_train, y_train, 8 * strategy.num_replicas_in_sync, epochs=3, verbose=1,
-                      validation_data=(x_val, y_val))
-        model.save('data/bh_model')
+    h = model.fit(train_ds, epochs=3, verbose=1,
+                  validation_data=val_ds)
+    model.save('data/bh_model')
