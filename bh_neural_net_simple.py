523f8437d11f2c0a1f9d4794f6d0b005bbcefed6
Bryan <b.r.hill@outlook.com>
2023-03-30 20:12:50 -0700

    Change to much much simpler model

--- bh_neural_net_simple.py
+++ bh_neural_net_simple.py
@@ -3,49 +3,30 @@ import torch
 from transformers import RobertaModel
 
 
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self, data, tokenizer, seq_len=1500):
-        self.seq_len = seq_len
-        self.tokenizer = tokenizer
-        texts = data.text.values.tolist()
-        # add preprocess here?
-        self.texts = [self.tokenizer(text,
-                                     max_length=self.seq_len,
-                                     padding="max_length",
-                                     truncation=True,
-                                     return_tensors="pt")
-                      for text in texts]
-        labels = data.score.values.tolist()
-        self.labels = labels
-
-    def __len__(self):
-        return len(self.texts)
-
-    def __getitem__(self, idx):
-        text = self.texts[idx]
-        label = self.labels[idx]
-        return text, label
-
-
-class Classify(nn.Module):
+class RegressModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.r_model = RobertaModel.from_pretrained('roberta-base')
-        # may need to change in features
-        self.fc1 = nn.Linear(in_features=768, out_features=1)
-        self.fc2 = nn.Linear(32, 1)
-        self.relu = nn.ReLU()
-        self.sig = nn.Sigmoid()
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features=101, out_features=25),
+            nn.ReLU(),
+            nn.Linear(in_features=25, out_features=10),
+            nn.ReLU(),
+            nn.Linear(in_features=10, out_features=4)
+        )
 
-    def forward(self, in_ids, attention_mask):
-        """
-        Passed input ids and attention mask from tokenization
-        raw is the CLS token of the last hidden layer
-        """
-        raw = self.r_model(in_ids, attention_mask)[0][:, 0]
-        out = self.fc1(raw)
-        out = self.relu(out)
+    def forward(self, x):
+        return self.linear_layer_stack(x)
 
-        out = self.fc2(out)
-        out = self.sig(out)
-        return out
+
+class ClassifyModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features=101, out_features=25),
+            nn.Linear(in_features=25, out_features=5)
+        )
+
+    def forward(self, x):
+        return self.linear_layer_stack(x)
+
+
--- neural_net_train.py
+++ neural_net_train.py
@@ -1,18 +1,37 @@
-import numpy
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding, TrainingArguments, Trainer
-from sklearn import metrics
-from datasets import load_dataset
+import copy
 
-def pre(text):
-    return tokenizer(text['text'], truncation=True, padding=True)
+import numpy as np
+from sklearn import metrics
+import pandas as pd
+import tqdm
+from joblib import load, dump
+from bh_neural_net_simple import RegressModel, ClassifyModel
+import torch
+import random
+
+from sklearn.model_selection import train_test_split
+
+CLASSIFICATION_LABELS = ["stars"]
+TARGET_LABELS = ["funny", "useful", "cool", "stars"]
+MODEL_FILE_PREFIX_CLF = "./models/bh_clf_"
+MODEL_FILE_PREFIX_RGR = "./models/bh_rgr_"
+target = "stars"
+DEVICE = "cpu"
+
+
+def accuracy_function(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()  # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
 
 def met(pred):
     predict, labels = pred
-    predict = numpy.argmax(predict, axis=1)
+    predict = np.argmax(predict, axis=1)
     precision = metrics.precision_score(labels, predict, average='weighted')
     recall = metrics.recall_score(labels, predict, average='weighted')
     f1 = metrics.f1_score(labels, predict, average='weighted')
-    ac = metrics.accuracy_score(labels, predict, average='weighted')
+    ac = metrics.accuracy_score(labels, predict)
     return {
         "Accuracy": ac,
         "Precision": precision,
@@ -20,39 +39,92 @@ def met(pred):
         "F1": f1
     }
 
+
 if __name__ == "__main__":
 
-    train_df = load_dataset("csv", data_files="data/neural_train.csv")
-    train_df = train_df["train"]
-    train_df = train_df.remove_columns(["useful", "cool", "funny"])
-    train_df = train_df.train_test_split(test_size=0.2)
-    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
-    token_train = train_df.map(pre, batched=True)
-    dc = DataCollatorWithPadding(tokenizer=tokenizer)
-    id2label = {0: "One", 1: "Two", 2: "Three", 3: "Four", 4: "Five"}
-    label2id = {"One": 0, "Two": 1, "Three": 2, "Four": 3, "Five": 4}
-    model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=5,
-                                                               )
+    # Read 1% of the data for fast testing.
+    # TODO change this to read the whole training set when you are ready.
+    training = pd.read_csv("data/output.csv", header=0, skiprows=lambda i: i > 0 and random.random() > 0.01)
 
-    train_arg = TrainingArguments(
-        output_dir="star_model",
-        learning_rate=1e-5,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
-        num_train_epochs=2,
-        weight_decay=0.01,
-        evaluation_strategy="epoch",
-        save_strategy="epoch",
-        load_best_model_at_end=True
-    )
 
-    trainer = Trainer(
-        model=model,
-        args=train_arg,
-        train_dataset=token_train["train"],
-        eval_dataset=token_train["test"],
-        tokenizer=tokenizer,
-        compute_metrics=met,
-    )
+    # Convert to tensor
+    X_t = training.drop(["stars", "funny", "useful", "cool"], axis=1).values
+    y_t = training[["stars"]].values
+    y_t = y_t - 1
+    x_train, x_test, y_train, y_test = train_test_split(X_t, y_t, train_size=0.7, shuffle=True)
+    x_train = torch.from_numpy(x_train).type(torch.float)
+    y_train = torch.from_numpy(y_train).type(torch.LongTensor)
+    x_test = torch.from_numpy(x_test).type(torch.float)
+    y_test = torch.from_numpy(y_test).type(torch.LongTensor)
 
-    trainer.train()
+    # 1. Create NN model.
+    model = ClassifyModel().to(DEVICE)
+
+    # 2. Create loss function - cross entropy loss
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    # 3. Create optimizer - stochastic gradient descent
+    # lr == learning rate == 0.1
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+    # 4. Train NN model.
+    torch.manual_seed(42)
+    # NOTE: change epochs to something like 20-50 most likely
+    # when training on the whole training set.
+    # It's probably going to be slow.
+    epochs = 100
+    batch = 10
+    batch_s = torch.arange(0, len(x_train), batch)
+    X = x_train.to(DEVICE)
+    y = torch.flatten(y_train.to(DEVICE))
+    x_test = x_test.to(DEVICE)
+    y_test = torch.flatten(y_test.to(DEVICE))
+
+    best_mse = np.inf
+    best_weights = None
+    hist = []
+
+    for epoch in range(epochs):
+        """
+        model.train()
+        with tqdm.tqdm(batch_s, unit="batch", mininterval=0, disable=True) as bar:
+            bar.set_description(f"Epoch {epoch}")
+            for start in bar:
+                x_b = X[start:start + batch]
+                y_b = y[start:start + batch]
+                y_l = model(x_b)
+                loss = loss_fn(y_l, y_b)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                bar.set_postfix(mse=float(loss))
+        model.eval()
+        y_l = model(x_test)
+        y_pred = torch.softmax(y_l, dim=1).argmax(dim=1)
+        mse = loss_fn(y_l, y_test)
+        mse = float(mse)
+        hist.append(mse)
+        acc = accuracy_function(y_true=y_test, y_pred=y_pred)
+        if mse < best_mse:
+            best_mse = mse
+            best_weights = copy.deepcopy(model.linear_layer_stack.state_dict())
+        """
+        model.train()
+
+        y_logits = model(X)
+        y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
+
+        loss = loss_fn(y_logits, y)
+        acc = accuracy_function(y_true=y, y_pred=y_pred)
+
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+        model.eval()
+        if epoch % 10 == 0:
+            print(f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}%")
+    #model.load_state_dict(best_weights)
+    dump(model, f"{MODEL_FILE_PREFIX_CLF}{target}.joblib")
