5113fc73cff9c9b9cc337ce08d18066f1dc40792
Bryan <b.r.hill@outlook.com>
2023-03-31 16:52:49 -0700

    init

--- neural_net_process.py
+++ neural_net_process.py
@@ -1,9 +1,10 @@
-from pandas import read_csv
+from pandas import DataFrame, read_csv, concat, to_numeric
+
 
 train_df = read_csv("data/training.csv")
+train_df = train_df.sample(10000)
 n = len(train_df["stars"])
 train_df = train_df.rename(columns={"stars": "label"})
 train_df = train_df.dropna()
 train_df["label"] = train_df["label"].apply(lambda x : int(x) - 1)
-
-train_df.to_csv("data/neural_train.csv", index_label=False)
+train_df.to_csv("data/small_neural_train.csv", index_label=False)
--- neural_net_train.py
+++ neural_net_train.py
@@ -4,7 +4,7 @@ from sklearn import metrics
 from datasets import load_dataset
 
 def pre(text):
-    return tokenizer(text['text'], truncation=True, padding=True)
+    return tokenizer(text['text'], truncation=True)
 
 def met(pred):
     predict, labels = pred
@@ -12,7 +12,7 @@ def met(pred):
     precision = metrics.precision_score(labels, predict, average='weighted')
     recall = metrics.recall_score(labels, predict, average='weighted')
     f1 = metrics.f1_score(labels, predict, average='weighted')
-    ac = metrics.accuracy_score(labels, predict, average='weighted')
+    ac = metrics.accuracy_score(labels, predict)
     return {
         "Accuracy": ac,
         "Precision": precision,
@@ -21,24 +21,22 @@ def met(pred):
     }
 
 if __name__ == "__main__":
-
-    train_df = load_dataset("csv", data_files="data/neural_train.csv")
+    train_df = load_dataset("csv", data_files="data/small_neural_train.csv")
     train_df = train_df["train"]
-    train_df = train_df.remove_columns(["useful", "cool", "funny"])
+    train_df = train_df.remove_columns(["__index_level_0__","useful", "cool", "funny"])
     train_df = train_df.train_test_split(test_size=0.2)
     tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
-    token_train = train_df.map(pre, batched=True)
+    token_train = train_df["train"].map(pre, batched=True)
+    token_val = train_df["test"].map(pre, batched=True)
     dc = DataCollatorWithPadding(tokenizer=tokenizer)
-    id2label = {0: "One", 1: "Two", 2: "Three", 3: "Four", 4: "Five"}
-    label2id = {"One": 0, "Two": 1, "Three": 2, "Four": 3, "Five": 4}
     model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=5,
                                                                )
 
     train_arg = TrainingArguments(
         output_dir="star_model",
-        learning_rate=1e-5,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
+        learning_rate=2e-5,
+        per_device_train_batch_size=64,
+        per_device_eval_batch_size=64,
         num_train_epochs=2,
         weight_decay=0.01,
         evaluation_strategy="epoch",
@@ -49,10 +47,13 @@ if __name__ == "__main__":
     trainer = Trainer(
         model=model,
         args=train_arg,
-        train_dataset=token_train["train"],
-        eval_dataset=token_train["test"],
+        train_dataset=token_train,
+        eval_dataset=token_val,
         tokenizer=tokenizer,
+        data_collator=dc,
         compute_metrics=met,
     )
 
     trainer.train()
+    print(trainer.evaluate())
+    trainer.save_model("models/bh_classify")
