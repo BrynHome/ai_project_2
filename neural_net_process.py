514b0780529b30ce8e9c4ed6adb96c71c6f9d2d0
Bryan <b.r.hill@outlook.com>
2023-04-02 20:22:18 -0700

    Small adjustment to neural_net_processing and requirements

--- neural_net_process.py
+++ neural_net_process.py
@@ -3,12 +3,15 @@ from argparse import ArgumentParser
 
 REGRESSION_LABELS = ["funny", "useful", "cool"]
 PARSER = ArgumentParser()
-PARSER.add_argument("sample", help="Number of entries to sample from the train set.", type=int)
+PARSER.add_argument("sample", help="Number of entries to sample from the train set.", type=int, required=False)
 ARGS = PARSER.parse_args()
-SAMPLE_AMOUNT = ARGS.sample
+
+
 
 train_df = read_csv("data/training.csv")
-train_df = train_df.sample(SAMPLE_AMOUNT)
+if ARGS.sample:
+	SAMPLE_AMOUNT = ARGS.sample 
+	train_df = train_df.sample(SAMPLE_AMOUNT)
 for label in REGRESSION_LABELS:
 	train_df[label] = train_df[label].apply(lambda x : float(x))
 train_df = train_df.dropna()
